6958ba7ed53b5e9ef3cf93a07829506595a49b42
Ubuntu <ubuntu@domU-12-31-39-12-FA-2F.compute-1.internal>
2010-11-27 04:25:38 +0000

    Aaaand we have a persistent web interface.  This is even actually usable ish, just ill-featured.

--- server.js
+++ server.js
@@ -23,15 +23,38 @@ process.on('exit', function() {
 process.on('SIGINT', process.exit);
 process.on('SIGTERM', process.exit);
 
+var commands = {
+    store: function(req) {
+        database[req.row.id] = req.row;
+        return {};
+    },
+    dump: function(req) {
+        return database;
+    },    
+};
+
+function errorOut(res, msg) {
+    res.writeHead(500, {'Content-type': 'text/plain'});
+    res.end(msg);
+}
+
 http.createServer(function (req, res) {
     console.log("Got URL: " + req.url);
-    var storeRow = /store\?(.*)/.exec(req.url);
-    if (storeRow) {
-        var storeReq = querystring.parse(storeRow[1]);
-        console.log("Req = " + JSON.stringify(storeReq));
-        database[storeReq.row.id] = storeReq.row;
-        res.writeHead(200, {'Content-type': 'text/javascript'});
-        res.end(storeReq.callback + "(\"success\")");
-        return;
+    
+    var match = /^\/(\w+)\?(.*)/.exec(req.url);
+    if (match) {
+        var req = querystring.parse(match[2]);
+        var cmd = commands[match[1]];
+        if (cmd) {
+            var ret = cmd(req);
+            res.writeHead(200, {'Content-type': 'text/javascript'});
+            res.end(req.callback + "(" + JSON.stringify(ret) + ")");
+        }
+        else {
+            errorOut(res, "Invalid command");
+        }
+    }
+    else {
+        errorOut(res, "Invalid URL");
     }
 }).listen(8080, '0.0.0.0');
